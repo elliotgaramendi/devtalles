2d51c1e000a0f33cc41df4434fc8158e4649a0f3
elliotgaramendi <elliotgaramendi@gmail.com>
2024-06-24 17:46:28 -0500

    refactor: Convert RN-01 variables to an object

--- react-native/01-react-foundations/src/components/BusinessCard.tsx
+++ react-native/01-react-foundations/src/components/BusinessCard.tsx
@@ -1,14 +1,22 @@
 const BusinessCard = () => {
-  const name = 'Elliot Garamendi';
-  const age = 28;
-  const isActive = true;
-  const skills = ['React', 'React Native', 'Next.js', 'Astro'];
+  const person = {
+    firstName: 'Elliot',
+    lastName: 'Garamendi',
+    age: 28,
+    isAlive: true,
+    skills: ['React', 'React Native', 'Next.js', 'Astro'],
+    address: {
+      country: 'Perú',
+      city: 'Lima'
+    }
+  };
 
   return (
     <div>
       <h2>Tarjeta de presentación</h2>
-      <h3>{name} <sup>{age}</sup><sub>{isActive ? 'true' : 'false'}</sub></h3>
-      <h4>{skills.join(', ')}</h4>
+      <h3>{person.firstName} {person.lastName} <sup>{person.age}</sup><sub>{person.isAlive ? 'true' : 'false'}</sub></h3>
+      <h4>{person.address.city} {person.address.country}</h4>
+      <h5>{person.skills.join(', ')}</h5>
     </div>
   );
 };
