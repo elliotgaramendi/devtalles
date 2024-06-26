eb0aaa9fdffa0c4fcf69cdb493b68737fb704b2a
elliotgaramendi <elliotgaramendi@gmail.com>
2024-06-26 06:16:31 -0500

    refactor: Improve RN-01 project

--- react-native/01-react-foundations/src/App.tsx
+++ react-native/01-react-foundations/src/App.tsx
@@ -2,7 +2,6 @@ import './App.css';
 import Auth from './components/Auth';
 import BusinessCard from './components/BusinessCard';
 import Forms from './components/Forms';
-import Likes from './components/Likes';
 import Users from './components/Users';
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
     <main>
       <h1>React Foundations</h1>
       <BusinessCard />
-      <Likes />
       <Auth />
       <Users />
       <Forms />
--- react-native/01-react-foundations/src/components/BusinessCard.tsx
+++ react-native/01-react-foundations/src/components/BusinessCard.tsx
@@ -1,3 +1,5 @@
+import Likes from "./Likes";
+
 interface Address {
   country: string;
   city: string;
@@ -30,12 +32,12 @@ const BusinessCard = () => {
   };
 
   return (
-    <div>
+    <section>
       <h2>Business Card</h2>
-      <h3>{person.firstName} {person.lastName} <sup>{person.age}</sup><sub>{person.isAlive ? 'true' : 'false'}</sub> <small>{isAdult(person.age) ? '🟢' : '🔴'}</small></h3>
-      <h4>{person.address?.city} {person.address?.country}</h4>
-      <h5>{person.skills.join(', ')}</h5>
-    </div>
+      <h3>{person.firstName} <sup>{person.age}</sup><sub>{person.isAlive ? 'true' : 'false'}</sub> <small>{isAdult(person.age) ? '🟢' : '🔴'}</small></h3>
+      <h4>{person.skills.join(', ')}</h4>
+      <Likes />
+    </section>
   );
 };
 
