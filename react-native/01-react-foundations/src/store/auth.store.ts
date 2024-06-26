f2c83b042fa593818cae7686dcd44c757c7a699e
elliotgaramendi <elliotgaramendi@gmail.com>
2024-06-26 04:46:31 -0500

    feat: Update RN-01 auth store

--- react-native/01-react-foundations/src/store/auth.store.ts
+++ react-native/01-react-foundations/src/store/auth.store.ts
@@ -6,11 +6,38 @@ interface AuthState {
   user?: {
     name: string;
     email: string;
-  }
+  };
+  signIn: (email: string, password: string) => void;
+  signOut: () => void;
 }
 
-export const useAuthStore = create<AuthState>()(() => ({
+export const useAuthStore = create<AuthState>()((set) => ({
   status: 'checking',
   token: undefined,
-  user: undefined
+  user: undefined,
+  signIn: (email, password) => {
+    if (password === '123456') {
+      set({
+        status: 'authenticated',
+        token: 'CKT294',
+        user: {
+          name: 'Elliot Garamendi',
+          email
+        }
+      });
+    } else {
+      set({
+        status: 'unauthenticated',
+        token: undefined,
+        user: undefined
+      });
+    }
+  },
+  signOut: () => {
+    set({
+      status: 'unauthenticated',
+      token: undefined,
+      user: undefined
+    });
+  },
 }));
