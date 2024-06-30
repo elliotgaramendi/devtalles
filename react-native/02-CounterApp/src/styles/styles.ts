ed62385d7777ba152e6bff6e91a86e2d22dbd682
elliotgaramendi <elliotgaramendi@gmail.com>
2024-06-30 11:39:00 -0500

    style: Create RN-02 styles

---
+++ react-native/02-CounterApp/src/styles/styles.ts
@@ -0,0 +1,42 @@
+import {StyleSheet} from 'react-native';
+
+export const primaryColor = '#23B5E8';
+export const blackColor = '#010508';
+export const primaryBackground = blackColor;
+export const lightPrimaryText = blackColor;
+export const size = 3.5;
+
+export const globalStyles = StyleSheet.create({
+  body: {
+    backgroundColor: primaryBackground,
+    height: '100%',
+  },
+});
+
+export const layoutStyles = StyleSheet.create({
+  container: {
+    paddingHorizontal: size * 4,
+  },
+});
+
+export const componentStyles = StyleSheet.create({
+  header: {
+    paddingVertical: size * 2,
+  },
+  section: {
+    paddingVertical: size * 8,
+  },
+});
+
+export const elementStyles = StyleSheet.create({
+  title: {
+    color: primaryColor,
+    fontSize: size * 8,
+    fontWeight: '800',
+  },
+});
+
+export const utilStyles = StyleSheet.create({
+  dFlex: {display: 'flex'},
+  aItemsCenter: {alignItems: 'center'},
+});
