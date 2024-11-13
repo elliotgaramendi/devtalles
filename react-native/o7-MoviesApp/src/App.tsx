a02580b8b94ec74ca32478ade3e23b2104c5a334
elliotgaramendi <elliotgaramendi@gmail.com>
2024-11-13 01:26:42 -0500

    chore: Set up RN-o7 initial project structure

---
+++ react-native/o7-MoviesApp/src/App.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/react-in-jsx-scope */
+import { Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;
---
+++ react-native/o7-MoviesApp/src/presentation/screens/details/DetailsScreen.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/react-in-jsx-scope */
+import { Text, View } from 'react-native';
+
+const DetailsScreen = () => {
+  return (
+    <View>
+      <Text>Details Screen</Text>
+    </View>
+  );
+};
+
+export default DetailsScreen;
