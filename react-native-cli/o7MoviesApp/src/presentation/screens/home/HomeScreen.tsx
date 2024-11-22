e28edfa6e419208e7d4c5319523d708425f9ae84
elliotgaramendi <elliotgaramendi@gmail.com>
2024-11-21 20:50:09 -0500

    feat: Update RNC-o7 Home screen

--- react-native-cli/o7MoviesApp/src/presentation/screens/home/HomeScreen.tsx
+++ react-native-cli/o7MoviesApp/src/presentation/screens/home/HomeScreen.tsx
@@ -1,11 +1,29 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { Text, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { componentStyles } from '../../../styles/styles';
+import PosterCarousel from '../../components/movies/PosterCarousel';
+import useMovies from '../../hooks/useMovies';
 
 const HomeScreen = () => {
+  const { top } = useSafeAreaInsets();
+  const { isLoading, nowPlaying } = useMovies();
+
+  if (isLoading) {
+    return (
+      <View style={[componentStyles.section]}>
+        <Text>Cargando...</Text>
+      </View>
+    );
+  }
+
   return (
-    <View>
-      <Text>Home Screen</Text>
-    </View>
+    <ScrollView>
+      <View style={[componentStyles.section, { marginTop: top }]}>
+        <PosterCarousel movies={nowPlaying} />
+      </View>
+    </ScrollView>
   );
 };
 
