571e2f665c7bc81ed3557549270bd2caedad6068
elliotgaramendi <elliotgaramendi@gmail.com>
2024-11-28 17:43:16 -0500

    feat: Update RNC-o7 Details screen

--- react-native-cli/o7MoviesApp/src/presentation/screens/details/DetailsScreen.tsx
+++ react-native-cli/o7MoviesApp/src/presentation/screens/details/DetailsScreen.tsx
@@ -1,11 +1,49 @@
 /* eslint-disable react/react-in-jsx-scope */
+import { useNavigation } from '@react-navigation/native';
+import { StackScreenProps } from '@react-navigation/stack';
 import { Text, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import { componentStyles, elementStyles, utilStyles } from '../../../styles/styles';
+import MovieDetail from '../../components/movie/MovieDetail';
+import MovieHeader from '../../components/movie/MovieHeader';
+import useMovie from '../../hooks/useMovie';
+import { RootStackParamList } from '../../navigation/Navigation';
 
-const DetailsScreen = () => {
+interface Props extends StackScreenProps<RootStackParamList, 'Details'> { }
+
+const DetailsScreen = ({ route }: Props) => {
+  const { movieId } = route.params;
+  const navigation = useNavigation();
+  const { isLoading, movie, movieCast } = useMovie(movieId);
+
+
+  if (isLoading || movie === undefined || movieCast === undefined) {
+    return (
+      <View style={[componentStyles.section]}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
   return (
-    <View>
-      <Text>Details Screen</Text>
-    </View>
+    <>
+      <Text
+        onPress={() => navigation.goBack()}
+        style={[elementStyles.title, utilStyles.pAbsolute, utilStyles.top2, utilStyles.left2, utilStyles.zIndex1]}
+      >
+        ⬅️
+      </Text>
+      <ScrollView>
+        <MovieHeader
+          originalTitle={movie.originalTitle}
+          title={movie.title}
+          poster={movie.poster}
+          rating={movie.rating}
+          genres={movie.genres}
+        />
+        <MovieDetail movie={movie} movieCast={movieCast} />
+      </ScrollView>
+    </>
   );
 };
 
