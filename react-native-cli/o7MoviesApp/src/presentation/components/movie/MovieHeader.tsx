07a69cd194129acd1838e3033cf4bdbc276b2723
elliotgaramendi <elliotgaramendi@gmail.com>
2024-11-28 17:40:34 -0500

    feat: Update RNC-o7 MovieHeader component

--- react-native-cli/o7MoviesApp/src/presentation/components/movie/MovieHeader.tsx
+++ react-native-cli/o7MoviesApp/src/presentation/components/movie/MovieHeader.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable react/react-in-jsx-scope */
-import { useNavigation } from '@react-navigation/native';
 import { Image, Text, useWindowDimensions, View } from 'react-native';
 import { elementStyles, layoutStyles, utilStyles } from '../../../styles/styles';
 
@@ -7,21 +6,15 @@ interface Props {
   originalTitle?: string;
   title?: string;
   poster?: string;
+  rating?: number;
+  genres?: string[];
 }
 
-
-const MovieHeader = ({ originalTitle, title, poster }: Props) => {
+const MovieHeader = ({ originalTitle, title, poster, rating, genres }: Props) => {
   const { height: screenHeight } = useWindowDimensions();
-  const navigation = useNavigation();
 
   return (
     <View style={utilStyles.gap4}>
-      <Text
-        onPress={() => navigation.goBack()}
-        style={[elementStyles.title, utilStyles.pAbsolute, utilStyles.top2, utilStyles.left2, utilStyles.zIndex1]}
-      >
-        ⬅️
-      </Text>
       <Image
         source={{ uri: poster }}
         style={[elementStyles.image, elementStyles.imageIsBannerPoster, { height: screenHeight * 0.75 }]}
@@ -29,6 +22,7 @@ const MovieHeader = ({ originalTitle, title, poster }: Props) => {
       <View style={layoutStyles.container}>
         <Text style={[elementStyles.text, utilStyles.cSecondary]}>{title}</Text>
         <Text style={[elementStyles.title, elementStyles.titleIsXS]}>{originalTitle}</Text>
+        <Text style={[elementStyles.text]}>{rating} - {genres?.join(', ')}</Text>
       </View>
     </View>
   );
