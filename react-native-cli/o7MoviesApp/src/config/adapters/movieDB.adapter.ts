c629696dfa37f8e20b9115ebe3fc78081b8e13bb
elliotgaramendi <elliotgaramendi@gmail.com>
2024-11-28 17:55:59 -0500

    chore: Update RNC-o7 environment variables config

--- react-native-cli/o7MoviesApp/src/config/adapters/movieDB.adapter.ts
+++ react-native-cli/o7MoviesApp/src/config/adapters/movieDB.adapter.ts
@@ -1,9 +1,10 @@
+import { BACKEND_API_KEY } from '@env';
 import { AxiosAdapter } from './http/axios.adapter';
 
 export const movieDBFetcher = new AxiosAdapter({
   baseURL: 'https://api.themoviedb.org/3/movie',
   params: {
-    api_key: process.env.BACKEND_API_KEY || '',
+    api_key: BACKEND_API_KEY || '',
     // language: 'es',
   },
 });
