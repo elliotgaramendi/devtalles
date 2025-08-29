871701753d99e7909f7809e5f74f9f6cb684e190
elliotgaramendi <elliotgaramendi@gmail.com>
2025-08-29 12:36:38 -0500

    feat: Wrap App with AuthProvider and integrate page (RNE o1)

--- react-native-expo/o1-react-foundations/src/App.tsx
+++ react-native-expo/o1-react-foundations/src/App.tsx
@@ -1,16 +1,23 @@
 import './App.css';
+import AuthPage from './components/AuthPage';
 import BusinessCard from './components/ProfileCard';
+import { AuthProvider } from './context/AuthContext';
 import useFollow from './hooks/useFollow';
 
 function App() {
   const { followers, increaseFollowersBy } = useFollow({ initialValue: 0 });
 
   return (
-    <main>
-      <section className="flex items-center justify-center h-dvh">
-        <BusinessCard followers={followers} onFollowersChange={increaseFollowersBy} />
-      </section>
-    </main>
+    <AuthProvider>
+      <main>
+        <section className="flex min-h-dvh py-16">
+          <div className="flex flex-col place-items-center place-content-center gap-2 md:flex-row container mx-auto px-4">
+            <BusinessCard followers={followers} onFollowersChange={increaseFollowersBy} />
+            <AuthPage />
+          </div>
+        </section>
+      </main>
+    </AuthProvider>
   )
 }
 
