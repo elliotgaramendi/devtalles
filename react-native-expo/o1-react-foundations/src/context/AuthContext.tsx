acf29b325abc417875e914d0ee19902a13e33e1c
elliotgaramendi <elliotgaramendi@gmail.com>
2025-08-29 12:31:43 -0500

    feat: Refactor AuthContext to manage authentication (RNE o1)

--- react-native-expo/o1-react-foundations/src/context/AuthContext.tsx
+++ react-native-expo/o1-react-foundations/src/context/AuthContext.tsx
@@ -1,11 +1,22 @@
-import { createContext, useContext, type PropsWithChildren } from "react";
+/* eslint-disable react-refresh/only-export-components */
+import { createContext, useContext, useEffect, useState, type PropsWithChildren } from "react";
+
+type AuthStatus = 'checking' | 'authenticated' | 'unauthenticated';
 
 interface AuthState {
-  user: {
-    name: string;
-    email: string;
-    image: string;
-  };
+  status: AuthStatus;
+  isChecking: boolean;
+  isAuthenticated: boolean;
+  token?: string;
+  user?: User | null;
+  signIn: (email: string, password: string) => void;
+  signOut: () => void;
+}
+
+interface User {
+  name: string;
+  email: string;
+  image: string;
 }
 
 export const AuthContext = createContext({} as AuthState);
@@ -13,13 +24,39 @@ export const AuthContext = createContext({} as AuthState);
 export const useAuthContext = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }: PropsWithChildren) => {
+  const [status, setStatus] = useState<AuthStatus>('checking');
+  const [user, setUser] = useState<User | null>(null);
+
+  const signIn = (email: string, password: string) => {
+    if (email === 'elliotgaramendi@base2.com' && password === 'theMostSecretPassword') {
+      setUser({ name: 'Elliot Garamendi', email, image: 'https://avatars.githubusercontent.com/elliotgaramendi' });
+      setStatus('authenticated');
+    } else {
+      alert('Invalid credentials');
+    }
+  };
+
+  const signOut = () => {
+    setUser(null);
+    setStatus('unauthenticated');
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setStatus('unauthenticated');
+    }, 1500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <AuthContext.Provider value={{
-      user: {
-        name: 'Elliot Garamendi',
-        email: 'elliotgaramendi@gmail.com',
-        image: 'https://avatars.githubusercontent.com/elliotgaramendi',
-      },
+      status,
+      isChecking: status === 'checking',
+      isAuthenticated: status === 'authenticated',
+      user,
+      signIn,
+      signOut,
     }}>
       {children}
     </AuthContext.Provider>
