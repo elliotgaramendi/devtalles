bf479002dbda6d5116314f29cb03e92883908123
elliotgaramendi <elliotgaramendi@gmail.com>
2025-08-31 22:12:46 -0500

    feat: Authentication management for users (RNE o1)

--- react-native-expo/o1-react-foundations/src/ui/pages/Home.tsx
+++ react-native-expo/o1-react-foundations/src/ui/pages/Home.tsx
@@ -1,7 +1,7 @@
 import useFollow from "../../hooks/useFollow";
 import BusinessCard from "../components/ProfileCard";
 import UserTableSection from "../modules/UserTableSection";
-import AuthPage from "../widgets/AuthWidget";
+import AuthWidget from "../widgets/AuthWidget";
 
 const Home = () => {
   const { followers, increaseFollowersBy } = useFollow({ initialValue: 0 });
@@ -10,7 +10,7 @@ const Home = () => {
     <>
       <section className="py-16">
         <div className="grid place-items-center place-content-center gap-8 md:grid-cols-2 container mx-auto px-4">
-          <AuthPage />
+          <AuthWidget />
           <BusinessCard followers={followers} onFollowersChange={increaseFollowersBy} />
         </div>
       </section>
--- react-native-expo/o1-react-foundations/src/ui/widgets/AuthWidget.tsx
+++ react-native-expo/o1-react-foundations/src/ui/widgets/AuthWidget.tsx
@@ -1,10 +1,32 @@
+import { useForm } from "react-hook-form";
 import { useAuthContext } from "../../context/AuthContext";
 
-const AuthPage = () => {
+type FormInputs = {
+  email: string;
+  password: string;
+};
+
+const AuthWidget = () => {
+  const { register, handleSubmit } = useForm<FormInputs>({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit = (myForm: FormInputs) => {
+    const { email, password } = myForm;
+    signIn(email, password);
+  };
+
   const { isChecking, isAuthenticated, user, signIn, signOut } = useAuthContext();
 
   if (isChecking) {
-    return <h3>Checking... 🤔</h3>;
+    return (
+      <div className="py-4">
+        <h3>Checking... 🤔</h3>
+      </div>
+    );
   }
 
   return (
@@ -23,17 +45,37 @@ const AuthPage = () => {
       ) : (
         <div className="flex flex-col gap-1 items-center">
           <h3>Enter the app 🔑</h3>
-          <button
-            className="py-2 px-4 bg-sky-400 text-gray-900 rounded-lg font-medium cursor-pointer hover:bg-sky-500 transition"
-            onClick={() => signIn('elliotgaramendi@base2.com', 'theMostSecretPassword')}
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="max-w-md min-w-2xs mx-auto"
           >
-            Sign in
-          </button>
+            <div className="mb-5">
+              <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+              <input
+                type="email"
+                id="email"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="egaramendi@base2.com"
+                required
+                {...register('email', { required: true })}
+              />
+            </div>
+            <div className="mb-5">
+              <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+              <input
+                type="password"
+                id="password"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required
+                {...register('password', { required: true })}
+              />
+            </div>
+            <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign in</button>
+          </form>
         </div>
-      )
-      }
+      )}
     </div>
   );
-}
+};
 
-export default AuthPage;
+export default AuthWidget;
