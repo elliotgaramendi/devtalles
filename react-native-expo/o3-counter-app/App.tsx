2b9ae23d02e9350536b6d7cd90a48cdd837139fc
elliotgaramendi <elliotgaramendi@gmail.com>
2025-09-04 20:54:38 -0500

    feat: update core functionality in app (RNE o3)

--- react-native-expo/o3-counter-app/App.tsx
+++ react-native-expo/o3-counter-app/App.tsx
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import Button from './components/Button';
+import { styles } from './styles/styles';
 
 export default function App() {
   const [count, setCount] = useState(0);
@@ -9,47 +11,24 @@ export default function App() {
     <View style={styles.hero}>
       <View style={styles.container}>
         <Text style={styles.title}>{count}</Text>
-        <Pressable
-          style={styles.button}
-          onPress={() => setCount(count + 1)}
-          onLongPress={() => setCount(0)}
-        >
-          <Text style={[styles.interactive, styles.interactiveIs2XL]}>+1</Text>
-        </Pressable>
+        <View style={[styles.fDirectionRow, styles.gap1]}>
+          <Button
+            type='buttonIsPrimary'
+            content="-1"
+            contentStyles={[styles.interactive, styles.interactiveIs2XL]}
+            onPress={() => setCount(count - 1)}
+            onLongPress={() => setCount(0)}
+          />
+          <Button
+            type='buttonIsPrimary'
+            content="+1"
+            contentStyles={[styles.interactive, styles.interactiveIs2XL]}
+            onPress={() => setCount(count + 1)}
+            onLongPress={() => setCount(0)}
+          />
+        </View>
         <StatusBar style="auto" />
       </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  hero: {
-    flex: 1,
-    backgroundColor: '#010508',
-  },
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    color: '#fefefe',
-    fontSize: 60,
-    fontWeight: '900',
-  },
-  interactive: {
-    color: '#010508',
-    fontSize: 14,
-    fontWeight: '600',
-  },
-  interactiveIs2XL: {
-    fontSize: 20,
-    fontWeight: '700',
-  },
-  button: {
-    paddingBlock: 8,
-    paddingInline: 16,
-    borderRadius: 8,
-    backgroundColor: '#23b5e8',
-  },
-});
