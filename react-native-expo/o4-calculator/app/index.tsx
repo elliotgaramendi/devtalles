5b5e8c1e5bb999ee82776fa4ba8c38b2dfe25008
elliotgaramendi <elliotgaramendi@gmail.com>
2025-09-12 17:33:35 -0500

    feat: Update HomeScreen logic (RNE o4)

--- react-native-expo/o4-calculator/app/index.tsx
+++ react-native-expo/o4-calculator/app/index.tsx
@@ -1,9 +1,24 @@
 import { Text, View } from "react-native";
 
 import AppButton from "@/components/AppButton";
+import useCalculator from "@/hooks/useCalculator";
 import { componentStyles, elementStyles, layoutStyles, utilsStyles } from "@/styles/styles";
 
 const HomeScreen = () => {
+  const {
+    formula,
+    prevNumber,
+    buildNumber,
+    clearNumber,
+    toggleSign,
+    delNumber,
+    addOperation,
+    subtractOperation,
+    multiplyOperation,
+    divideOperation,
+    calculateResult
+  } = useCalculator();
+
   return (
     <View style={componentStyles.hero}>
       <View style={[layoutStyles.container, utilsStyles.aItemsCenter, utilsStyles.jContentEnd, utilsStyles.gap4]}>
@@ -13,14 +28,14 @@ const HomeScreen = () => {
             numberOfLines={1}
             adjustsFontSizeToFit
           >
-            5 x 8
+            {formula}
           </Text>
           <Text
             style={[elementStyles.title, utilsStyles.cSecondaryText]}
             numberOfLines={1}
             adjustsFontSizeToFit
           >
-            40
+            {formula !== prevNumber && formula.includes(' ') === true && prevNumber}
           </Text>
         </View>
         <View style={[utilsStyles.fDirectionRow, utilsStyles.fWrap, utilsStyles.jContentCenter, utilsStyles.gap4, utilsStyles.widthSm]}>
@@ -28,121 +43,121 @@ const HomeScreen = () => {
             type="button_secondary"
             content="C"
             contentStyles={[elementStyles.title, elementStyles.title_2xs]}
-            onPress={() => { }}
+            onPress={clearNumber}
           />
           <AppButton
             type="button_secondary"
             content="+/-"
             contentStyles={[elementStyles.title, elementStyles.title_2xs]}
-            onPress={() => { }}
+            onPress={toggleSign}
           />
           <AppButton
             type="button_secondary"
             content="del"
             contentStyles={[elementStyles.title, elementStyles.title_2xs]}
-            onPress={() => { }}
+            onPress={delNumber}
           />
           <AppButton
             type="button_primary"
             content="/"
             contentStyles={[elementStyles.title, elementStyles.title_2xs, utilsStyles.cLightPrimaryText]}
-            onPress={() => { }}
+            onPress={divideOperation}
           />
           <AppButton
             type="button_secondaryBg"
             content="7"
             contentStyles={[elementStyles.title, elementStyles.title_2xs]}
-            onPress={() => { }}
+            onPress={() => buildNumber("7")}
           />
           <AppButton
             type="button_secondaryBg"
             content="8"
             contentStyles={[elementStyles.title, elementStyles.title_2xs]}
-            onPress={() => { }}
+            onPress={() => buildNumber("8")}
           />
           <AppButton
             type="button_secondaryBg"
             content="9"
             contentStyles={[elementStyles.title, elementStyles.title_2xs]}
-            onPress={() => { }}
+            onPress={() => buildNumber("9")}
           />
           <AppButton
             type="button_primary"
             content="*"
             contentStyles={[elementStyles.title, elementStyles.title_2xs, utilsStyles.cLightPrimaryText]}
-            onPress={() => { }}
+            onPress={multiplyOperation}
           />
           <AppButton
             type="button_secondaryBg"
             content="4"
             contentStyles={[elementStyles.title, elementStyles.title_2xs]}
-            onPress={() => { }}
+            onPress={() => buildNumber("4")}
           />
           <AppButton
             type="button_secondaryBg"
             content="5"
             contentStyles={[elementStyles.title, elementStyles.title_2xs]}
-            onPress={() => { }}
+            onPress={() => buildNumber("5")}
           />
           <AppButton
             type="button_secondaryBg"
             content="6"
             contentStyles={[elementStyles.title, elementStyles.title_2xs]}
-            onPress={() => { }}
+            onPress={() => buildNumber("6")}
           />
           <AppButton
             type="button_primary"
             content="-"
             contentStyles={[elementStyles.title, elementStyles.title_2xs, utilsStyles.cLightPrimaryText]}
-            onPress={() => { }}
+            onPress={subtractOperation}
           />
           <AppButton
             type="button_secondaryBg"
             content="1"
             contentStyles={[elementStyles.title, elementStyles.title_2xs]}
-            onPress={() => { }}
+            onPress={() => buildNumber("1")}
           />
           <AppButton
             type="button_secondaryBg"
             content="2"
             contentStyles={[elementStyles.title, elementStyles.title_2xs]}
-            onPress={() => { }}
+            onPress={() => buildNumber("2")}
           />
           <AppButton
             type="button_secondaryBg"
             content="3"
             contentStyles={[elementStyles.title, elementStyles.title_2xs]}
-            onPress={() => { }}
+            onPress={() => buildNumber("3")}
           />
           <AppButton
             type="button_primary"
             content="+"
             contentStyles={[elementStyles.title, elementStyles.title_2xs, utilsStyles.cLightPrimaryText]}
-            onPress={() => { }}
+            onPress={addOperation}
           />
           <AppButton
             type="button_secondary"
             content="."
             contentStyles={[elementStyles.title, elementStyles.title_2xs]}
-            onPress={() => { }}
+            onPress={() => buildNumber(".")}
           />
           <AppButton
             type="button_secondaryBg"
             content="0"
             contentStyles={[elementStyles.title, elementStyles.title_2xs]}
-            onPress={() => { }}
+            onPress={() => buildNumber("0")}
           />
           <AppButton
             type="button_secondary"
             content="."
             contentStyles={[elementStyles.title, elementStyles.title_2xs]}
-            onPress={() => { }}
+            onPress={() => buildNumber(".")}
           />
           <AppButton
             type="button_primary"
             content="="
             contentStyles={[elementStyles.title, elementStyles.title_2xs, utilsStyles.cLightPrimaryText]}
-            onPress={() => { }}
+            onPress={calculateResult}
           />
         </View>
       </View>
