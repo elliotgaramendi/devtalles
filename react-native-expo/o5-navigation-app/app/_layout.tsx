c9040bec6f6055783e5b394dc1f8420987a5709b
elliotgaramendi <elliotgaramendi@gmail.com>
2025-09-15 11:18:49 -0500

    chore: Add custom fonts to NativeWind (RNE o5)

--- react-native-expo/o5-navigation-app/app/_layout.tsx
+++ react-native-expo/o5-navigation-app/app/_layout.tsx
@@ -1,7 +1,27 @@
-import { Stack } from "expo-router";
+import { useFonts } from 'expo-font';
+import { SplashScreen, Stack } from "expo-router";
 
+import { useEffect } from 'react';
 import '../styles/global.css';
 
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+
+  const [fontsLoaded, error] = useFonts({
+    'WorkSans-Black': require('../assets/fonts/WorkSans-Black.ttf'),
+    'WorkSans-Light': require('../assets/fonts/WorkSans-Light.ttf'),
+    'WorkSans-Medium': require('../assets/fonts/WorkSans-Medium.ttf'),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded && !error) return null;
+
+
   return <Stack />;
 }
--- react-native-expo/o5-navigation-app/tailwind.config.js
+++ react-native-expo/o5-navigation-app/tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./ui/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'work-black': ['WorkSans-Black', 'sans-serif'],
+        'work-bold': ['WorkSans-Bold', 'sans-serif'],
+        'work-medium': ['WorkSans-Medium', 'sans-serif'],
+        'work-regular': ['WorkSans-Regular', 'sans-serif'],
+        'work-light': ['WorkSans-Light', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
