a8961bf31b043f2b7a5ae12ba4b675201a16b665
elliotgaramendi <elliotgaramendi@gmail.com>
2025-09-15 11:36:13 -0500

    chore: Add custom colors to NativeWind (RNE o5)

--- react-native-expo/o5-navigation-app/app/index.tsx
+++ react-native-expo/o5-navigation-app/app/index.tsx
@@ -3,8 +3,20 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-work-black text-blue-500">
+      <Text className="text-2xl font-work-black text-blue-500">
         Welcome to Nativewind!
+      </Text>
+      <Text className="text-xl font-work-bold text-primary">
+        This is a app
+      </Text>
+      <Text className="text-lg font-work-light text-secondary">
+        With react-native
+      </Text>
+      <Text className="text-md font-work-medium text-secondary-100">
+        And react-native
+      </Text>
+      <Text className="text-sm font-work-regular text-tertiary">
+        I love this app
       </Text>
     </View>
   );
