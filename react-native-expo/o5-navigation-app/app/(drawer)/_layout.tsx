5239fef8ba97a80921661fe2825ec9e55dd3bcb7
elliotgaramendi <elliotgaramendi@gmail.com>
2025-09-22 11:22:26 -0500

    feat: update StatusBar (RNE o5)

--- react-native-expo/o5-navigation-app/app/(drawer)/_layout.tsx
+++ react-native-expo/o5-navigation-app/app/(drawer)/_layout.tsx
@@ -2,54 +2,58 @@ import { styleVariables } from '@/styles/styles';
 import CustomDrawer from '@/ui/components/shared/CustomDrawer';
 import { Ionicons } from '@expo/vector-icons';
 import { Drawer } from 'expo-router/drawer';
+import { StatusBar } from 'expo-status-bar';
 
 const DrawerLayout = () => {
   return (
-    <Drawer
-      drawerContent={CustomDrawer}
-      screenOptions={{
-        drawerActiveBackgroundColor: styleVariables.primaryColor,
-        drawerActiveTintColor: styleVariables.blackColor,
-        drawerInactiveTintColor: styleVariables.neutral200,
-        drawerStyle: { backgroundColor: styleVariables.blackColor, },
-        headerStyle: { backgroundColor: styleVariables.blackColor },
-        headerTintColor: styleVariables.primaryColor,
-        headerTitleStyle: { color: styleVariables.primaryColor },
-        sceneStyle: { backgroundColor: styleVariables.blackColor },
-      }}
-    >
-      <Drawer.Screen
-        name="(tabs)"
-        options={{
-          headerShown: false,
-          drawerLabel: 'Tabs',
-          title: 'Tabs',
-          drawerIcon: ({ color, size }: { color: string; size: number }) => (
-            <Ionicons size={size} name="albums" color={color} />
-          ),
+    <>
+      <StatusBar style="light" />
+      <Drawer
+        drawerContent={CustomDrawer}
+        screenOptions={{
+          drawerActiveBackgroundColor: styleVariables.primaryColor,
+          drawerActiveTintColor: styleVariables.blackColor,
+          drawerInactiveTintColor: styleVariables.neutral200,
+          drawerStyle: { backgroundColor: styleVariables.blackColor, },
+          headerStyle: { backgroundColor: styleVariables.blackColor },
+          headerTintColor: styleVariables.primaryColor,
+          headerTitleStyle: { color: styleVariables.primaryColor },
+          sceneStyle: { backgroundColor: styleVariables.blackColor },
         }}
-      />
-      <Drawer.Screen
-        name="user/index"
-        options={{
-          drawerLabel: 'User',
-          title: 'Users',
-          drawerIcon: ({ color, size }: { color: string; size: number }) => (
-            <Ionicons size={size} name="person" color={color} />
-          ),
-        }}
-      />
-      <Drawer.Screen
-        name="schedule/index"
-        options={{
-          drawerLabel: 'Schedule',
-          title: 'Schedule',
-          drawerIcon: ({ color, size }: { color: string; size: number }) => (
-            <Ionicons size={size} name="calendar" color={color} />
-          ),
-        }}
-      />
-    </Drawer>
+      >
+        <Drawer.Screen
+          name="(tabs)"
+          options={{
+            headerShown: false,
+            drawerLabel: 'Tabs',
+            title: 'Tabs',
+            drawerIcon: ({ color, size }: { color: string; size: number }) => (
+              <Ionicons size={size} name="albums" color={color} />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="user/index"
+          options={{
+            drawerLabel: 'User',
+            title: 'Users',
+            drawerIcon: ({ color, size }: { color: string; size: number }) => (
+              <Ionicons size={size} name="person" color={color} />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="schedule/index"
+          options={{
+            drawerLabel: 'Schedule',
+            title: 'Schedule',
+            drawerIcon: ({ color, size }: { color: string; size: number }) => (
+              <Ionicons size={size} name="calendar" color={color} />
+            ),
+          }}
+        />
+      </Drawer>
+    </>
   );
 }
 
