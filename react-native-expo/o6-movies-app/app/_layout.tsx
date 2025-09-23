780d6fc7861a3c99e6c3b3908d04046df3c45081
elliotgaramendi <elliotgaramendi@gmail.com>
2025-09-23 13:36:01 -0500

    feat: Add QueryClientProvider to RootLayout (RNE o6)

--- react-native-expo/o6-movies-app/app/_layout.tsx
+++ react-native-expo/o6-movies-app/app/_layout.tsx
@@ -1,7 +1,14 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Stack } from "expo-router";
 
 import '../styles/global.css';
 
+const queryClient = new QueryClient();
+
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Stack />
+    </QueryClientProvider>
+  );
 }
