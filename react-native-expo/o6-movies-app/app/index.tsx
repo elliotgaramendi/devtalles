0b0c85f7f72067940ab3495d3ba1ebe569700f5a
elliotgaramendi <elliotgaramendi@gmail.com>
2025-09-23 18:56:20 -0500

    feat: Update Home with modules  (RNE o6)

--- react-native-expo/o6-movies-app/app/index.tsx
+++ react-native-expo/o6-movies-app/app/index.tsx
@@ -1,15 +1,34 @@
-import { Text, View } from "react-native";
+import MainSlideshow from "@/presentation/components/movies/MainSlideshow";
+import MovieHorizontalList from "@/presentation/components/movies/MovieHorizontalList";
+import { useMovies } from "@/presentation/hooks/useMovies";
+import { ActivityIndicator, ScrollView, View } from "react-native";
 
 export default function Index() {
+  const { nowPlayingQuery, popularQuery, topRatedQuery, upcomingQuery } = useMovies();
+
+  if (nowPlayingQuery.isLoading) {
+    return (
+      <View className="justify-center items-center flex-1">
+        <ActivityIndicator color="purple" size={40} />
+      </View>
+    );
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <ScrollView>
+      <MainSlideshow movies={nowPlayingQuery.data ?? []} />
+      <MovieHorizontalList
+        title="Popular"
+        movies={popularQuery.data ?? []}
+      />
+      <MovieHorizontalList
+        title="Best rated"
+        movies={topRatedQuery.data ?? []}
+      />
+      <MovieHorizontalList
+        title="Upcoming"
+        movies={upcomingQuery.data ?? []}
+      />
+    </ScrollView>
   );
 }
