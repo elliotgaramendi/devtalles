565b91f36d3a0451eed4ad4234ad79f9b27bab23
elliotgaramendi <elliotgaramendi@gmail.com>
2025-09-23 18:57:26 -0500

    feat: Update Stack screenOptions (RNE o6)

--- react-native-expo/o6-movies-app/app/_layout.tsx
+++ react-native-expo/o6-movies-app/app/_layout.tsx
@@ -1,14 +1,25 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Stack } from "expo-router";
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import '../styles/global.css';
 
 const queryClient = new QueryClient();
 
 export default function RootLayout() {
+  const safeArea = useSafeAreaInsets();
+
   return (
     <QueryClientProvider client={queryClient}>
-      <Stack />
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          contentStyle: {
+            marginTop: safeArea.top,
+            backgroundColor: '#fefefe'
+          }
+        }}
+      />
     </QueryClientProvider>
   );
 }
