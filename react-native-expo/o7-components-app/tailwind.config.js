8d8fa7c15ea4dce396f7402b2fbbfc87041fd65b
elliotgaramendi <elliotgaramendi@gmail.com>
2025-11-03 14:14:41 -0500

    styles: Add Colors for light and dark themes (RNE o7)

--- react-native-expo/o7-components-app/tailwind.config.js
+++ react-native-expo/o7-components-app/tailwind.config.js
@@ -1,9 +1,28 @@
+import { Colors } from './constants/Colors';
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./presentation/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light: {
+          primary: Colors.light.primary,
+          secondary: Colors.light.secondary,
+          tertiary: Colors.light.tertiary,
+          background: Colors.light.background,
+          text: Colors.light.text,
+        },
+        dark: {
+          primary: Colors.dark.primary,
+          secondary: Colors.dark.secondary,
+          tertiary: Colors.dark.tertiary,
+          background: Colors.dark.background,
+          text: Colors.dark.text,
+        },
+      },
+    },
   },
   plugins: [],
 }
