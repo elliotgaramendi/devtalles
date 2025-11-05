ba5fddfda4033aed70e9e0550f010826a7fb17a3
elliotgaramendi <elliotgaramendi@gmail.com>
2025-11-05 12:31:26 -0500

    feat: Add fade animations to screen animation 101 (RNE o7)

--- react-native-expo/o7-components-app/app/animation-101/index.tsx
+++ react-native-expo/o7-components-app/app/animation-101/index.tsx
@@ -1,11 +1,41 @@
-import { Text, View } from 'react-native';
+import { useRef } from 'react';
+import { Animated } from 'react-native';
+
+import ThemedButton from '@/presentation/shared/ThemedButton';
+import ThemedView from '@/presentation/shared/ThemedView';
 
 const Animation101Screen = () => {
+  const animatedOpacity = useRef(new Animated.Value(0)).current;
+
+  const fadeIn = () => {
+    Animated.timing(animatedOpacity, {
+      toValue: 1,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const fadeOut = () => {
+    Animated.timing(animatedOpacity, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
+
   return (
-    <View>
-      <Text>Animation101Screen</Text>
-    </View>
+    <ThemedView margin className="justify-center items-center flex-1">
+      <Animated.View
+        className="bg-light-secondary dark:bg-dark-secondary rounded-xl"
+        style={{
+          width: 150,
+          height: 150,
+          opacity: animatedOpacity
+        }}
+      />
+      <ThemedButton className="my-5" onPress={() => fadeIn()}>FadeIn </ThemedButton>
+      <ThemedButton className="my-5" onPress={() => fadeOut()}>FadeOut </ThemedButton>
+    </ThemedView>
   );
 };
-
 export default Animation101Screen;
