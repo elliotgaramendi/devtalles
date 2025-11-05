e98d9d7b3e770437713c6982a159df6a1f632d93
elliotgaramendi <elliotgaramendi@gmail.com>
2025-11-05 15:05:39 -0500

    feat: Add useAnimation hook (RNE o7)

--- react-native-expo/o7-components-app/app/animation-101/index.tsx
+++ react-native-expo/o7-components-app/app/animation-101/index.tsx
@@ -1,34 +1,17 @@
-import { useRef } from 'react';
 import { Animated, Easing } from 'react-native';
 
+import { useAnimation } from '@/hooks/useAnimation';
 import ThemedButton from '@/presentation/shared/ThemedButton';
 import ThemedView from '@/presentation/shared/ThemedView';
 
 const Animation101Screen = () => {
-  const animatedOpacity = useRef(new Animated.Value(0)).current;
-  const animatedTop = useRef(new Animated.Value(-100)).current;
-
-  const fadeIn = () => {
-    Animated.timing(animatedOpacity, {
-      toValue: 1,
-      duration: 300,
-      useNativeDriver: true,
-    }).start();
-    Animated.timing(animatedTop, {
-      toValue: 0,
-      duration: 700,
-      useNativeDriver: true,
-      easing: Easing.bounce,
-    }).start();
-  };
-
-  const fadeOut = () => {
-    Animated.timing(animatedOpacity, {
-      toValue: 0,
-      duration: 300,
-      useNativeDriver: true,
-    }).start(() => animatedTop.resetAnimation());
-  };
+  const {
+    animatedOpacity,
+    animatedTop,
+    fadeIn,
+    fadeOut,
+    startMovingTopPosition,
+  } = useAnimation();
 
   return (
     <ThemedView margin className="justify-center items-center flex-1">
@@ -38,15 +21,28 @@ const Animation101Screen = () => {
           width: 150,
           height: 150,
           opacity: animatedOpacity,
-          transform: [
-            {
-              translateY: animatedTop,
-            },
-          ],
+          transform: [{
+            translateY: animatedTop,
+          }]
         }}
       />
-      <ThemedButton className="my-5" onPress={() => fadeIn()}>FadeIn </ThemedButton>
-      <ThemedButton className="my-5" onPress={() => fadeOut()}>FadeOut </ThemedButton>
+
+      <ThemedButton
+        className="my-5"
+        onPress={() => {
+          fadeIn({});
+          startMovingTopPosition({
+            easing: Easing.bounce,
+            duration: 700,
+          });
+        }}
+      >
+        FadeIn
+      </ThemedButton>
+
+      <ThemedButton className="my-5" onPress={() => fadeOut({})}>
+        FadeOut
+      </ThemedButton>
     </ThemedView>
   );
 };
