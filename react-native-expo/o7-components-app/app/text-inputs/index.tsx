c55f2810751458e243658736c9dc603dbd8c2207
elliotgaramendi <elliotgaramendi@gmail.com>
2025-11-10 17:40:20 -0500

    feat: Update TextInputsScreen (RNE o7)

--- react-native-expo/o7-components-app/app/text-inputs/index.tsx
+++ react-native-expo/o7-components-app/app/text-inputs/index.tsx
@@ -1,11 +1,31 @@
-import { Text, View } from 'react-native';
+import ThemedCard from '@/presentation/shared/ThemedCard';
+import ThemedText from '@/presentation/shared/ThemedText';
+import ThemedTextInput from '@/presentation/shared/ThemedTextInput';
+import ThemedView from '@/presentation/shared/ThemedView';
+import { useState } from 'react';
 
 const TextInputsScreen = () => {
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    phone: '',
+  });
+
   return (
-    <View>
-      <Text>TextInputsScreen</Text>
-    </View>
+    <ThemedView margin>
+      <ThemedCard className="mb-5">
+        <ThemedTextInput
+          placeholder="Nombre Completo"
+          autoCapitalize={'words'}
+          autoCorrect={false}
+          onChangeText={(text) => setForm({ ...form, name: text })}
+        />
+      </ThemedCard>
+
+      <ThemedCard className="my-2">
+        <ThemedText>{JSON.stringify(form, null, 2)}</ThemedText>
+      </ThemedCard>
+    </ThemedView>
   );
 };
-
 export default TextInputsScreen;
