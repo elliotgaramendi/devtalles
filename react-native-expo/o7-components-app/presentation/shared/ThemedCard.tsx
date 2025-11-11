d5bf9bcb9574580a79f8ca187a1aa687d8f51bf3
elliotgaramendi <elliotgaramendi@gmail.com>
2025-11-11 08:56:16 -0500

    fix: Add ...rest props to ThemedCard (RNE o7)

--- react-native-expo/o7-components-app/presentation/shared/ThemedCard.tsx
+++ react-native-expo/o7-components-app/presentation/shared/ThemedCard.tsx
@@ -4,14 +4,14 @@ interface Props extends ViewProps {
   className?: string;
 }
 
-const ThemedCard = ({ className, children }: Props) => {
+const ThemedCard = ({ className, children, ...rest }: Props) => {
   return (
     <View
       className={`bg-white dark:bg-black/10 rounded-xl p-2 shadow shadow-black/5 ${className}`}
+      {...rest}
     >
       {children}
     </View>
   );
 };
-
 export default ThemedCard;
