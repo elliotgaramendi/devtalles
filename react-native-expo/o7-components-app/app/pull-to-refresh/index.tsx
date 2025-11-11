c656ab00966816d1340f3417aa841b8457209dde
elliotgaramendi <elliotgaramendi@gmail.com>
2025-11-11 10:10:02 -0500

    feat: Update PullToRefreshScreen (RNE o7)

--- react-native-expo/o7-components-app/app/pull-to-refresh/index.tsx
+++ react-native-expo/o7-components-app/app/pull-to-refresh/index.tsx
@@ -1,11 +1,44 @@
-import { Text, View } from 'react-native';
+import { useThemeColor } from '@/hooks/use-theme-color';
+import ThemedText from '@/presentation/shared/ThemedText';
+import ThemedView from '@/presentation/shared/ThemedView';
+import { useState } from 'react';
+import { RefreshControl, ScrollView } from 'react-native';
 
 const PullToRefreshScreen = () => {
+  const primaryColor = useThemeColor({}, 'primary');
+  const backgroundColor = useThemeColor(
+    {
+      dark: 'black',
+      light: 'white',
+    },
+    'background'
+  );
+
+  const [isRefreshing, setIsRefreshing] = useState(false);
+
+  const onRefresh = async () => {
+    setIsRefreshing(true);
+
+    setTimeout(() => {
+      setIsRefreshing(false);
+    }, 3000);
+  };
+
   return (
-    <View>
-      <Text>PullToRefreshScreen</Text>
-    </View>
+    <ScrollView
+      refreshControl={
+        <RefreshControl
+          refreshing={isRefreshing}
+          onRefresh={onRefresh}
+          colors={[primaryColor, 'red', 'orange', 'green']}
+          progressBackgroundColor={backgroundColor}
+        />
+      }
+    >
+      <ThemedView margin>
+        <ThemedText className="bg-sky-500/10">PullToRefreshScreen</ThemedText>
+      </ThemedView>
+    </ScrollView>
   );
 };
-
 export default PullToRefreshScreen;
