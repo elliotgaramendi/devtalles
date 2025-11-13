d530b20488ff3a3bb3e03b83175f53c22e04f29d
elliotgaramendi <elliotgaramendi@gmail.com>
2025-11-13 12:36:11 -0500

    feat: Update main routes to base state (RNE o8)

--- react-native-expo/o8-push-app/app/_layout.tsx
+++ react-native-expo/o8-push-app/app/_layout.tsx
@@ -4,6 +4,7 @@ import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { useThemeColor } from '@/hooks/use-theme-color';
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -11,12 +12,19 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const backgroundColor = useThemeColor({}, 'background');
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+      <Stack
+        screenOptions={{
+          headerShadowVisible: false,
+          contentStyle: {
+            backgroundColor: backgroundColor
+          }
+        }}
+      >
+        <Stack.Screen name="index" options={{ title: 'Push App' }} />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
---
+++ react-native-expo/o8-push-app/app/index.tsx
@@ -0,0 +1,12 @@
+import { ThemedText } from '@/components/themed-text';
+import { View } from 'react-native';
+
+const PushApp = () => {
+  return (
+    <View>
+      <ThemedText>Hello World</ThemedText>
+    </View>
+  );
+}
+
+export default PushApp;
