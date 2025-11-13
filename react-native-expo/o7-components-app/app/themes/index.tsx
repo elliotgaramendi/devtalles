6817f994f9305c4b9622c76fdd80df3f937351a4
elliotgaramendi <elliotgaramendi@gmail.com>
2025-11-13 10:03:42 -0500

    feat: Update ThemesScreen (RNE o7)

--- react-native-expo/o7-components-app/app/themes/index.tsx
+++ react-native-expo/o7-components-app/app/themes/index.tsx
@@ -1,11 +1,51 @@
-import { Text, View } from 'react-native';
+import { useState } from 'react';
+
+import ThemedCard from '@/presentation/shared/ThemedCard';
+import ThemedSwitch from '@/presentation/shared/ThemedSwitch';
+import ThemedView from '@/presentation/shared/ThemedView';
+import { useColorScheme } from 'nativewind';
 
 const ThemesScreen = () => {
+  const { colorScheme, setColorScheme } = useColorScheme();
+
+  const [darkModeSettings, setDarkModeSettings] = useState({
+    darkMode: colorScheme === 'dark',
+    systemMode: false,
+  });
+
+  const setDarkMode = (value: boolean) => {
+    setColorScheme(value ? 'dark' : 'light');
+
+    setDarkModeSettings({
+      darkMode: value,
+      systemMode: false,
+    });
+  };
+
+  const setSystemMode = (value: boolean) => {
+    setDarkModeSettings({
+      darkMode: darkModeSettings.darkMode,
+      systemMode: value,
+    });
+  };
+
   return (
-    <View>
-      <Text>ThemesScreen</Text>
-    </View>
+    <ThemedView margin>
+      <ThemedCard className="mt-5">
+        <ThemedSwitch
+          text="Dark Mode"
+          className="mb-5"
+          value={darkModeSettings.darkMode}
+          onValueChange={setDarkMode}
+        />
+
+        <ThemedSwitch
+          text="System Mode"
+          value={darkModeSettings.systemMode}
+          onValueChange={setSystemMode}
+        />
+      </ThemedCard>
+    </ThemedView>
   );
 };
-
 export default ThemesScreen;
