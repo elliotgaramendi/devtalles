2254b441d94eb4038bf378abacf5e6d9841479bf
elliotgaramendi <elliotgaramendi@gmail.com>
2025-11-13 10:04:24 -0500

    feat: Update RootLayout (RNE o7)

--- react-native-expo/o7-components-app/app/_layout.tsx
+++ react-native-expo/o7-components-app/app/_layout.tsx
@@ -1,38 +1,57 @@
-import { Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+import { SplashScreen, Stack } from 'expo-router';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import { allRoutes } from '@/constants/Routes';
 import { useThemeColor } from '@/hooks/use-theme-color';
+import { ThemeChangerProvider } from '@/presentation/context/ThemeChangerContext';
+import { useEffect } from 'react';
 import '../global.css';
 
 export default function RootLayout() {
   const backgroundColor = useThemeColor({}, 'background');
   const textColor = useThemeColor({}, 'text');
 
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <GestureHandlerRootView style={{ flex: 1, backgroundColor }}>
-      <Stack
-        screenOptions={{
-          headerShadowVisible: false,
-          contentStyle: { backgroundColor, },
-          headerStyle: { backgroundColor },
-          headerTintColor: textColor,
-        }}
-      >
-        <Stack.Screen
-          name="index"
-          options={{ title: 'Home' }}
-        />
-        {allRoutes.map((route) => (
+      <ThemeChangerProvider>
+        <Stack
+          screenOptions={{
+            headerShadowVisible: false,
+            contentStyle: { backgroundColor, },
+            headerStyle: { backgroundColor },
+            headerTintColor: textColor,
+          }}
+        >
           <Stack.Screen
-            key={route.name}
-            name={route.name}
-            options={{
-              title: route.title,
-            }}
+            name="index"
+            options={{ title: 'Home' }}
           />
-        ))}
-      </Stack>
+          {allRoutes.map((route) => (
+            <Stack.Screen
+              key={route.name}
+              name={route.name}
+              options={{
+                title: route.title,
+              }}
+            />
+          ))}
+        </Stack>
+      </ThemeChangerProvider>
     </GestureHandlerRootView>
   );
 }
