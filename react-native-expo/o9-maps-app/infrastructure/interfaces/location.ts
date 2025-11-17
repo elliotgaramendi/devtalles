04a61e49deeb819d5ea4a540293be00d090b8639
elliotgaramendi <elliotgaramendi@gmail.com>
2025-11-17 17:42:30 -0500

    feat: add location interfaces (RNE o9)

---
+++ react-native-expo/o9-maps-app/infrastructure/interfaces/location.ts
@@ -0,0 +1,10 @@
+
+export enum PermissionStatus {
+  CHECKING = 'checking',
+  GRANTED = 'GRANTED',
+  DENIED = 'DENIED',
+  BLOCKED = 'BLOCKED',
+  LIMITED = 'LIMITED',
+  UNAVAILABLE = 'UNAVAILABLE',
+  UNDETERMINED = 'UNDETERMINED',
+}
