db98a346473377ac188697abe42b53a905c4e2fa
elliotgaramendi <elliotgaramendi@gmail.com>
2025-11-17 11:55:26 -0500

    feat: Add push-notification service (RNE o8-b)

--- react-native-expo/o8-push-app-back/src/app.module.ts
+++ react-native-expo/o8-push-app-back/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PushNotificationService } from './push-notification/push-notification.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PushNotificationService],
 })
 export class AppModule {}
