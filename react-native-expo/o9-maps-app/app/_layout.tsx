e356819887953395ceae103bbeabf32fc2fb1a4d
elliotgaramendi <elliotgaramendi@gmail.com>
2025-11-18 10:02:08 -0500

    feat: wrap RootLayout in PermissionsCheckerProvider (RNE o9)

--- react-native-expo/o9-maps-app/app/_layout.tsx
+++ react-native-expo/o9-maps-app/app/_layout.tsx
@@ -4,6 +4,7 @@ import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/presentation/hooks/use-color-scheme';
+import PermissionsCheckerProvider from '@/presentation/providers/PermissionsCheckerProvider';
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -14,18 +15,20 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name="index" options={{ animation: 'none' }} />
-        <Stack.Screen name="loading/index" options={{ animation: 'none' }} />
-        <Stack.Screen name="map/index" options={{ animation: 'fade' }} />
-        <Stack.Screen name="permissions/index" options={{ animation: 'fade' }}
-        />
-      </Stack>
-      <StatusBar style="auto" />
+      <PermissionsCheckerProvider>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="index" options={{ animation: 'none' }} />
+          <Stack.Screen name="loading/index" options={{ animation: 'none' }} />
+          <Stack.Screen name="map/index" options={{ animation: 'fade' }} />
+          <Stack.Screen name="permissions/index" options={{ animation: 'fade' }}
+          />
+        </Stack>
+        <StatusBar style="auto" />
+      </PermissionsCheckerProvider>
     </ThemeProvider>
   );
 }
