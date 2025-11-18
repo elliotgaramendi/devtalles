7a65c3b9237d85e9eda1994c932f96f8bdbb2e05
elliotgaramendi <elliotgaramendi@gmail.com>
2025-11-18 10:04:15 -0500

    feat: update PermissionsScreen (RNE o9)

--- react-native-expo/o9-maps-app/app/permissions/index.tsx
+++ react-native-expo/o9-maps-app/app/permissions/index.tsx
@@ -1,11 +1,25 @@
-import { Text, View } from 'react-native';
+import { ThemedText } from '@/presentation/components/shared/themed-text';
+import { ThemedView } from '@/presentation/components/shared/themed-view';
+import ThemedPressable from '@/presentation/components/shared/ThemedPressable';
+import { usePermissionsStore } from '@/presentation/store/usePermissions';
 
 const PermissionsScreen = () => {
+  const { locationStatus, requestLocationPermission } = usePermissionsStore();
+
   return (
-    <View>
-      <Text>PermissionsScreen</Text>
-    </View>
+    <ThemedView
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <ThemedPressable onPress={requestLocationPermission}>
+        Habilitar ubicación
+      </ThemedPressable>
+
+      <ThemedText>Estado actual: {locationStatus}</ThemedText>
+    </ThemedView>
   );
 };
-
 export default PermissionsScreen;
