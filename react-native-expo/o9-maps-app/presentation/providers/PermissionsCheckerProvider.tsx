0e841a8748dda8f3835fddb97e1ee842b7c3ad2a
elliotgaramendi <elliotgaramendi@gmail.com>
2025-11-18 09:42:08 -0500

    feat: add PermissionsCheckerProvider (RNE o9)

---
+++ react-native-expo/o9-maps-app/presentation/providers/PermissionsCheckerProvider.tsx
@@ -0,0 +1,39 @@
+
+import { router } from 'expo-router';
+import { PropsWithChildren, useEffect } from 'react';
+import { AppState } from 'react-native';
+
+import { PermissionStatus } from '@/infrastructure/interfaces/location';
+
+import { usePermissionsStore } from '../store/usePermissions';
+
+const PermissionsCheckerProvider = ({ children }: PropsWithChildren) => {
+  const { locationStatus, checkLocationPermission } = usePermissionsStore();
+
+  useEffect(() => {
+    checkLocationPermission();
+  }, [checkLocationPermission]);
+
+  useEffect(() => {
+    if (locationStatus === PermissionStatus.GRANTED) {
+      router.replace('/map');
+    } else if (locationStatus !== PermissionStatus.CHECKING) {
+      router.replace('/permissions');
+    }
+  }, [locationStatus]);
+
+  useEffect(() => {
+    const subscription = AppState.addEventListener('change', (nextAppState) => {
+      if (nextAppState === 'active') {
+        checkLocationPermission();
+      }
+    });
+
+    return () => {
+      subscription.remove();
+    };
+  }, [checkLocationPermission]);
+
+  return <>{children}</>;
+};
+export default PermissionsCheckerProvider;
