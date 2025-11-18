2b26119e07f0cf833e80d97fa2a57e783f1e38d1
elliotgaramendi <elliotgaramendi@gmail.com>
2025-11-18 11:02:05 -0500

    feat: update MapScreen (RNE o9)

--- react-native-expo/o9-maps-app/app/map/index.tsx
+++ react-native-expo/o9-maps-app/app/map/index.tsx
@@ -1,12 +1,52 @@
-
-import { Text, View } from 'react-native';
+import { ThemedView } from '@/presentation/components/shared/themed-view';
+import { StyleSheet } from 'react-native';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 
 const MapScreen = () => {
   return (
-    <View>
-      <Text>Map Screen</Text>
-    </View>
+    <ThemedView style={styles.container}>
+      <MapView
+        // showsPointsOfInterest={false}
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: 45.41256,
+          longitude: -75.698931,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Marker
+          coordinate={{
+            latitude: 45.41256,
+            longitude: -75.698931,
+          }}
+          title="Aquí estoy"
+          description="Esta es mi casa por algún lado de Ottawa"
+        />
+
+        <Marker
+          coordinate={{
+            latitude: 45.434005,
+            longitude: -75.677708,
+          }}
+          title="Un parque"
+          description="Esta es mi casa por algún lado de Ottawa"
+        />
+      </MapView>
+    </ThemedView>
   );
 };
 
 export default MapScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+    // backgroundColor: 'red',
+  },
+});
