975a2025b6a8b6d86d981ed61e00970ff63d8dda
elliotgaramendi <elliotgaramendi@gmail.com>
2025-11-24 16:00:01 -0500

    feat: add CustomMap component (RNE o9)

---
+++ react-native-expo/o9-maps-app/presentation/components/maps/CustomMap.tsx
@@ -0,0 +1,120 @@
+import { useEffect, useRef, useState } from 'react';
+import { StyleSheet, View, ViewProps } from 'react-native';
+import MapView, { Polyline } from 'react-native-maps';
+
+import { LatLng } from '@/infrastructure/interfaces/lat-lng';
+import { useLocationStore } from '@/presentation/store/useLocationStore';
+import FAB from '../shared/FAB';
+
+interface Props extends ViewProps {
+  initialLocation: LatLng;
+  showUserLocation?: boolean;
+}
+
+const CustomMap = ({
+  initialLocation,
+  showUserLocation = true,
+  ...rest
+}: Props) => {
+  const mapRef = useRef<MapView>(null);
+  const [isFollowingUser, setIsFollowingUser] = useState(true);
+  const [showPolyline, setShowPolyline] = useState(false);
+
+  const { lastKnownLocation, userLocationList, getLocation, watchLocation, clearWatchLocation } = useLocationStore();
+
+  useEffect(() => {
+    watchLocation();
+
+    return () => {
+      clearWatchLocation();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    if (lastKnownLocation && isFollowingUser) {
+      moveCameraToLocation(lastKnownLocation);
+    }
+  }, [lastKnownLocation, isFollowingUser]);
+
+  const moveCameraToLocation = (latLng: LatLng) => {
+    if (!mapRef.current) return;
+
+    mapRef.current.animateCamera({
+      center: latLng,
+    });
+  };
+
+  const moveToCurrentLocation = async () => {
+    if (!lastKnownLocation) {
+      moveCameraToLocation(initialLocation);
+    } else {
+      moveCameraToLocation(lastKnownLocation);
+    }
+
+    const location = await getLocation();
+    if (!location) return;
+
+    moveCameraToLocation(location);
+  };
+
+  return (
+    <View {...rest}>
+      <MapView
+        ref={mapRef}
+        // showsPointsOfInterest={false}
+        onTouchStart={() => setIsFollowingUser(false)}
+        showsUserLocation={showUserLocation}
+        style={styles.map}
+        initialRegion={{
+          latitude: initialLocation.latitude,
+          longitude: initialLocation.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        {showPolyline && <Polyline
+          coordinates={userLocationList}
+          strokeColor={"black"}
+          strokeWidth={5}
+        />}
+      </MapView>
+
+      <FAB
+        iconName={showPolyline ? 'eye-outline' : 'eye-off-outline'}
+        onPress={() => setShowPolyline(!showPolyline)}
+        style={{
+          bottom: 140,
+          right: 20,
+        }}
+      />
+
+      <FAB
+        iconName={isFollowingUser ? 'walk-outline' : 'accessibility-outline'}
+        onPress={() => setIsFollowingUser(!isFollowingUser)}
+        style={{
+          bottom: 80,
+          right: 20,
+        }}
+      />
+
+      <FAB
+        iconName="compass-outline"
+        onPress={moveToCurrentLocation}
+        style={{
+          bottom: 20,
+          right: 20,
+        }}
+      />
+    </View>
+  );
+};
+export default CustomMap;
+
+const styles = StyleSheet.create({
+  map: {
+    width: '100%',
+    height: '100%',
+    // backgroundColor: 'red',
+  },
+});
