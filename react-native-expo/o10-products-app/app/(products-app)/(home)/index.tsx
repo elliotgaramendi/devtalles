b4f2a2ccb7e348f7481ecb8ab17fb0f4a94e2ca4
elliotgaramendi <elliotgaramendi@gmail.com>
2025-11-25 11:14:57 -0500

    chore: update app.json with identifiers and fonts (RNE o10 f)

--- react-native-expo/o10-products-app/app/(products-app)/(home)/index.tsx
+++ react-native-expo/o10-products-app/app/(products-app)/(home)/index.tsx
@@ -1,17 +1,16 @@
 import { ThemedText } from '@/presentation/theme/components/themed-text';
 import { useThemeColor } from '@/presentation/theme/hooks/use-theme-color';
 import { View } from 'react-native';
-
 const HomeScreen = () => {
-  const primary = useThemeColor({}, 'tint');
+  const primary = useThemeColor({}, 'primary');
 
   return (
     <View style={{ paddingTop: 100, paddingHorizontal: 20 }}>
-      <ThemedText style={{ fontFamily: 'KanitBold', color: primary }}>
+      <ThemedText style={{ fontFamily: 'Kanit-Bold', color: primary }}>
         HomeScreen
       </ThemedText>
-      <ThemedText style={{ fontFamily: 'KanitRegular' }}>HomeScreen</ThemedText>
-      <ThemedText style={{ fontFamily: 'KanitThin' }}>HomeScreen</ThemedText>
+      <ThemedText style={{ fontFamily: 'Kanit-Regular' }}>HomeScreen</ThemedText>
+      <ThemedText style={{ fontFamily: 'Kanit-Thin' }}>HomeScreen</ThemedText>
       <ThemedText>HomeScreen</ThemedText>
     </View>
   );
