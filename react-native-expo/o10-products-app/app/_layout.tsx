0f882bea29bba4cabbc4d30f14b09d61b6e7526f
elliotgaramendi <elliotgaramendi@gmail.com>
2025-12-04 14:55:23 -0500

    feat: update RootLayout with TanStack (RNE o10 f)

--- react-native-expo/o10-products-app/app/_layout.tsx
+++ react-native-expo/o10-products-app/app/_layout.tsx
@@ -1,4 +1,5 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
@@ -10,7 +11,17 @@ export const unstable_settings = {
   anchor: '(tabs)',
 };
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+});
+
 export default function RootLayout() {
+
+
   const colorScheme = useColorScheme();
   const backgroundColor = useThemeColor({}, 'background');
 
@@ -18,27 +29,29 @@ export default function RootLayout() {
     <GestureHandlerRootView
       style={{ backgroundColor: backgroundColor, flex: 1 }}
     >
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        <Stack
-          screenOptions={{
-            headerShown: false,
-          }}
-        >
-          <Stack.Screen
-            name="auth/login/index"
-            options={{
-              title: 'Ingresar',
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+          <Stack
+            screenOptions={{
+              headerShown: false,
             }}
-          />
-          <Stack.Screen
-            name="auth/register/index"
-            options={{
-              title: 'Crear cuenta',
-            }}
-          />
-        </Stack>
-        <StatusBar style="auto" />
-      </ThemeProvider>
+          >
+            <Stack.Screen
+              name="auth/login/index"
+              options={{
+                title: 'Ingresar',
+              }}
+            />
+            <Stack.Screen
+              name="auth/register/index"
+              options={{
+                title: 'Crear cuenta',
+              }}
+            />
+          </Stack>
+          <StatusBar style="auto" />
+        </ThemeProvider>
+      </QueryClientProvider>
     </GestureHandlerRootView>
   );
 }
