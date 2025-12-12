6194886eb1efc285a8bffe8a395bdf22f7b70913
elliotgaramendi <elliotgaramendi@gmail.com>
2025-12-12 18:03:24 -0500

    feat: update useProduct (RNE o10 f)

--- react-native-expo/o10-products-app/presentation/products/hooks/useProduct.ts
+++ react-native-expo/o10-products-app/presentation/products/hooks/useProduct.ts
@@ -1,11 +1,14 @@
 import { updateCreateProduct } from '@/core/products/actions/create-update-product.action';
 import { getProductById } from '@/core/products/actions/get-product-by-id.action';
 import { Product } from '@/core/products/interfaces/product.interface';
+import { useCameraStore } from '@/presentation/store/useCameraStore';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { useRef } from 'react';
 import { Alert } from 'react-native';
 
 export const useProduct = (productId: string) => {
+  const { clearImages } = useCameraStore();
+
   const queryClient = useQueryClient();
   const productIdRef = useRef(productId);
 
@@ -16,14 +19,15 @@ export const useProduct = (productId: string) => {
   });
 
   const productMutation = useMutation({
-    mutationFn: async (data: Product) =>
-      updateCreateProduct({
-        ...data,
-        id: productIdRef.current,
-      }),
+    mutationFn: async (data: Product) => updateCreateProduct({
+      ...data,
+      id: productIdRef.current,
+    }),
 
     onSuccess(data: Product) {
       productIdRef.current = data.id;
+
+      clearImages();
 
       queryClient.invalidateQueries({
         queryKey: ['products', 'infinite'],
