cfb8de1afc012f7c816a53a91aa3320d9f746de9
elliotgaramendi <elliotgaramendi@gmail.com>
2026-01-28 22:43:45 -0500

    fix: update component styles  (shadcn-ui)

--- shadcn-ui/shadcn-dashboard/app/dashboard/command/page.tsx
+++ shadcn-ui/shadcn-dashboard/app/dashboard/command/page.tsx
@@ -56,7 +56,7 @@ const CommandPage = () => {
   return (
     <section>
       <div className="flex flex-col gap-4">
-        <div className="mt-10 h-[200px] flex justify-center items-center">
+        <div className="mt-10 h-50 flex justify-center items-center">
           <p className="text-sm text-muted-foreground">
             Press{" "}
             <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
