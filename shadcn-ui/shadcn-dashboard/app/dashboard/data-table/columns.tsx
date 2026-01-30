0f8fadd2b267c36c4bbb7024d97ff5ecf4466add
elliotgaramendi <elliotgaramendi@gmail.com>
2026-01-30 00:17:11 -0500

    feat: add actions column with dropdown (shadcn-ui)

--- shadcn-ui/shadcn-dashboard/app/dashboard/data-table/columns.tsx
+++ shadcn-ui/shadcn-dashboard/app/dashboard/data-table/columns.tsx
@@ -1,8 +1,19 @@
 "use client"
 
-import { Badge } from "@/components/ui/badge"
-import { Payment } from "@/data/payments.data"
-import { ColumnDef } from "@tanstack/react-table"
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Payment } from "@/data/payments.data";
+import { ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal } from "lucide-react";
+import { toast } from 'sonner';
 
 const statusVariants = {
   pending: "outline",
@@ -48,6 +59,37 @@ export const columns: ColumnDef<Payment>[] = [
         >
           {value}
         </Badge>
+      )
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const payment = row.original
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => {
+                navigator.clipboard.writeText(payment.id);
+                toast("Payment ID copied to clipboard", { position: "top-right" });
+              }}
+            >
+              Copy payment ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       )
     },
   },
