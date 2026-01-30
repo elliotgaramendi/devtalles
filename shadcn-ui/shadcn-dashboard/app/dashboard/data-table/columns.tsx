482765f8452b8037083bc6d3e184c9ed898a8b3c
elliotgaramendi <elliotgaramendi@gmail.com>
2026-01-29 23:39:36 -0500

    feat: enhance data table columns (shadcn-ui)

--- shadcn-ui/shadcn-dashboard/app/dashboard/data-table/columns.tsx
+++ shadcn-ui/shadcn-dashboard/app/dashboard/data-table/columns.tsx
@@ -1,23 +1,54 @@
 "use client"
 
-import { Payment } from '@/data/payments.data'
+import { Badge } from "@/components/ui/badge"
+import { Payment } from "@/data/payments.data"
 import { ColumnDef } from "@tanstack/react-table"
+
+const statusVariants = {
+  pending: "outline",
+  processing: "info",
+  success: "success",
+  failed: "destructive",
+} as const
+
+type Status = keyof typeof statusVariants
 
 export const columns: ColumnDef<Payment>[] = [
   {
-    accessorKey: "amount",
-    header: "Amount",
-  },
-  {
-    accessorKey: "status",
-    header: "Status",
+    accessorKey: "clientName",
+    header: "Client Name",
   },
   {
     accessorKey: "email",
     header: "Email",
   },
   {
-    accessorKey: "clientName",
-    header: "Client Name",
-  }
+    accessorKey: "amount",
+    header: () => <div className="text-right">Amount</div>,
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue("amount"))
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(amount)
+
+      return <div className="text-right font-medium">{formatted}</div>
+    },
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+    cell: ({ row }) => {
+      const value = row.getValue("status") as Status
+
+      return (
+        <Badge
+          variant={statusVariants[value] || "default"}
+          capitalize
+        >
+          {value}
+        </Badge>
+      )
+    },
+  },
 ]
