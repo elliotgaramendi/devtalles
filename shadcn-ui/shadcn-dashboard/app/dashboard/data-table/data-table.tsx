71b116a33bd38fc2b0a2f0529a7e35597f352be3
elliotgaramendi <elliotgaramendi@gmail.com>
2026-02-03 09:57:34 -0500

    feat: update DataTable with filter status (shadcn-ui)

--- shadcn-ui/shadcn-dashboard/app/dashboard/data-table/data-table.tsx
+++ shadcn-ui/shadcn-dashboard/app/dashboard/data-table/data-table.tsx
@@ -4,6 +4,15 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
   Table,
   TableBody,
   TableCell,
@@ -37,6 +46,7 @@ export function DataTable<TData, TValue>({
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>(
     []
   );
+  const [currentStatus, setCurrentStatus] = useState("all");
 
   const table = useReactTable({
     data,
@@ -55,7 +65,7 @@ export function DataTable<TData, TValue>({
 
   return (
     <div>
-      <div className="flex items-center py-4">
+      <div className="flex flex-col md:flex-row items-center justify-between gap-4 py-4">
         <Input
           placeholder="Filter emails..."
           value={(table.getColumn("email")?.getFilterValue() as string) ?? ""}
@@ -64,6 +74,33 @@ export function DataTable<TData, TValue>({
           }
           className="max-w-sm"
         />
+        <Select
+          value={currentStatus}
+          onValueChange={(value) => {
+            if (value === "all") {
+              table.getColumn("status")?.setFilterValue(undefined);
+              setCurrentStatus("all");
+              return;
+            }
+
+            setCurrentStatus(value);
+            table.getColumn("status")?.setFilterValue(value);
+          }}
+        >
+          <SelectTrigger className="w-full max-w-48">
+            <SelectValue placeholder="All states" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Status</SelectLabel>
+              <SelectItem value="all">All states</SelectItem>
+              <SelectItem value="pending">Pending</SelectItem>
+              <SelectItem value="processing">Processing</SelectItem>
+              <SelectItem value="success">Success</SelectItem>
+              <SelectItem value="failed">Failed</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
       </div>
       <div className="overflow-hidden rounded-md border">
         <Table>
