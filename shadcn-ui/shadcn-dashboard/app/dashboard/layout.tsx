eb8cfce2d3fd47ae3fe89081cca63772329fb5e7
elliotgaramendi <elliotgaramendi@gmail.com>
2026-02-19 23:00:48 -0500

    feat: enhance layout for dark mode (shadcn-ui)

--- shadcn-ui/shadcn-dashboard/app/dashboard/layout.tsx
+++ shadcn-ui/shadcn-dashboard/app/dashboard/layout.tsx
@@ -47,108 +47,110 @@ export default function DashboardLayout({
     setSidebarOpen(!sidebarOpen);
   };
   return (
-    <header className="relative z-50">
-      <nav className="bg-white border-b border-gray-200 fixed z-30 w-full">
-        <div className="px-3 py-3 lg:px-5 lg:pl-3">
-          <div className="flex items-center justify-between">
-            <div className="flex items-center justify-start">
-              <button
-                onClick={toggleSidebar}
-                aria-expanded={sidebarOpen}
-                aria-controls="sidebar"
-                className="lg:hidden mr-2 text-gray-600 hover:text-gray-900 cursor-pointer p-2 hover:bg-gray-100 focus:bg-gray-100 focus:ring-2 focus:ring-gray-100 rounded"
-              >
-                <svg
-                  className={`w-6 h-6 ${sidebarOpen ? 'hidden' : ''}`}
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
+    <>
+      <header className="relative z-50">
+        <nav className="bg-white border-b border-gray-200 fixed z-30 w-full dark:bg-slate-900 dark:border-slate-700">
+          <div className="px-3 py-3 lg:px-5 lg:pl-3">
+            <div className="flex items-center justify-between">
+              <div className="flex items-center justify-start">
+                <button
+                  onClick={toggleSidebar}
+                  aria-expanded={sidebarOpen}
+                  aria-controls="sidebar"
+                  className="lg:hidden mr-2 text-gray-600 hover:text-gray-900 cursor-pointer p-2 hover:bg-gray-100 focus:bg-gray-100 focus:ring-2 focus:ring-gray-100 rounded"
                 >
-                  <path
-                    fillRule="evenodd"
-                    d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-                <svg
-                  className={`w-6 h-6 ${sidebarOpen ? '' : 'hidden'}`}
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
+                  <svg
+                    className={`w-6 h-6 ${sidebarOpen ? 'hidden' : ''}`}
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                  <svg
+                    className={`w-6 h-6 ${sidebarOpen ? '' : 'hidden'}`}
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                </button>
+                <Link
+                  href="/dashboard"
+                  className="text-xl font-bold flex items-center lg:ml-2.5"
                 >
-                  <path
-                    fillRule="evenodd"
-                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-              </button>
-              <Link
-                href="/dashboard"
-                className="text-xl font-bold flex items-center lg:ml-2.5"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 256 256"
-                  className="h-6 w-6"
-                >
-                  <rect width="256" height="256" fill="none"></rect>
-                  <line
-                    x1="208"
-                    y1="128"
-                    x2="128"
-                    y2="208"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="16"
-                  ></line>
-                  <line
-                    x1="192"
-                    y1="40"
-                    x2="40"
-                    y2="192"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="16"
-                  ></line>
-                </svg>
-                <span className="self-center whitespace-nowrap ml-2">
-                  {" "}
-                  Shadcn/ui
-                </span>
-              </Link>
-            </div>
-            <div className="flex items-center">
-              <Avatar>
-                <AvatarImage src="https://github.com/elliotgaramendi.png" alt="Elliot Garamendi" />
-                <AvatarFallback>EG</AvatarFallback>
-                <AvatarBadge className="bg-green-600 dark:bg-green-800" />
-              </Avatar>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 256 256"
+                    className="h-6 w-6"
+                  >
+                    <rect width="256" height="256" fill="none"></rect>
+                    <line
+                      x1="208"
+                      y1="128"
+                      x2="128"
+                      y2="208"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="16"
+                    ></line>
+                    <line
+                      x1="192"
+                      y1="40"
+                      x2="40"
+                      y2="192"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="16"
+                    ></line>
+                  </svg>
+                  <span className="self-center whitespace-nowrap ml-2">
+                    {" "}
+                    Shadcn/ui
+                  </span>
+                </Link>
+              </div>
+              <div className="flex items-center">
+                <Avatar>
+                  <AvatarImage src="https://github.com/elliotgaramendi.png" alt="Elliot Garamendi" />
+                  <AvatarFallback>EG</AvatarFallback>
+                  <AvatarBadge className="bg-green-600 dark:bg-green-800" />
+                </Avatar>
+              </div>
             </div>
           </div>
-        </div>
-      </nav>
-      <div className="flex overflow-hidden bg-white pt-16">
+        </nav>
+      </header>
+      <div className="flex overflow-hidden bg-white pt-16 dark:bg-slate-900">
         <aside
           id="sidebar"
           className={`fixed z-20 h-full top-0 left-0 pt-16 shrink-0 flex-col w-64 transition-all duration-300 ${sidebarOpen ? 'flex' : 'hidden lg:flex'
             }`}
           aria-label="Sidebar"
         >
-          <div className="relative flex-1 flex flex-col min-h-0 borderR border-gray-200 bg-white pt-0">
+          <div className="relative flex-1 flex flex-col min-h-0 borderR border-gray-200 bg-white pt-0 dark:bg-slate-900">
             <div className="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
-              <div className="flex-1 px-3 bg-white divide-y space-y-1">
+              <div className="flex-1 px-3 bg-white divide-y space-y-1 dark:bg-slate-900">
                 <ul className="space-y-2 pb-2">
                   {links.map((link) => (
                     <li key={link.href}>
                       <Link
                         href={link.href}
                         onClick={() => setSidebarOpen(false)}
-                        className="text-base capitalize text-gray-900 font-normal rounded-lg flex items-center p-2 hover:bg-gray-100 group"
+                        className="text-base capitalize text-gray-900 font-normal rounded-lg flex items-center p-2 hover:bg-gray-100 group dark:bg-slate-900 dark:hover:bg-slate-800 dark:text-slate-300"
                       >
                         <span className="ml-3">{link.name}</span>
                       </Link>
@@ -167,12 +169,12 @@ export default function DashboardLayout({
         ></div>
         <div
           id="main-content"
-          className="h-full w-full bg-gray-50 relative overflow-y-auto lg:ml-64"
+          className="h-full w-full bg-gray-50 relative overflow-y-auto lg:ml-64 dark:bg-black"
         >
           <main>
             <div className="pt-6 px-4">
               <div className="w-full min-h-[calc(100vh-230px)]">
-                <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8">
+                <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8 dark:bg-slate-900">
                   {children || (
                     <div className="text-center py-12">
                       <h2 className="text-2xl font-bold text-gray-800 mb-4">
@@ -187,7 +189,7 @@ export default function DashboardLayout({
               </div>
             </div>
           </main>
-          <footer className="bg-white md:flex md:items-center md:justify-between shadow rounded-lg p-4 md:p-6 xl:p-8 my-6 mx-4">
+          <footer className="bg-white dark:bg-slate-900 md:flex md:items-center md:justify-between shadow rounded-lg p-4 md:p-6 xl:p-8 my-6 mx-4">
             <ul className="flex items-center flex-wrap mb-6 md:mb-0">
               <li>
                 <a
@@ -308,6 +310,6 @@ export default function DashboardLayout({
           </p>
         </div>
       </div>
-    </header>
+    </>
   );
 }
