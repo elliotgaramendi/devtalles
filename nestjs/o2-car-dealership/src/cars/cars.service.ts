eb5497dc6b9524502baf82eccf0bae4f61c3f46d
Elliot Leo Garamendi Sarmiento <62267772+elliotgaramendi@users.noreply.github.com>
2026-08-20 10:33:55 -0500

    feat(nest-o2): implement cars CRUD module

---
+++ nestjs/o2-car-dealership/src/cars/cars.service.ts
@@ -0,0 +1,75 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { randomUUID } from 'node:crypto';
+
+import { CreateCarDto, UpdateCarDto } from './dto';
+import { Car } from './interfaces/car.interface';
+
+@Injectable()
+export class CarsService {
+  private cars: Car[] = [
+    {
+      id: randomUUID(),
+      brand: 'Toyota',
+      model: 'Corolla',
+    },
+    {
+      id: randomUUID(),
+      brand: 'Honda',
+      model: 'Civic',
+    },
+    {
+      id: randomUUID(),
+      brand: 'Jeep',
+      model: 'Cherokee',
+    },
+  ];
+
+  findAll() {
+    return this.cars;
+  }
+
+  findOneById(id: string) {
+    const car = this.cars.find((car) => car.id === id);
+    if (!car) throw new NotFoundException(`Car with id '${id}' not found`);
+
+    return car;
+  }
+
+  create(createCarDto: CreateCarDto) {
+    const newCar: Car = {
+      id: randomUUID(),
+      ...createCarDto,
+    };
+
+    this.cars.push(newCar);
+
+    return newCar;
+  }
+
+  update(id: string, updateCarDto: UpdateCarDto) {
+    const carDB = this.findOneById(id);
+
+    const changes = Object.fromEntries(
+      Object.entries(updateCarDto).filter(([, value]) => value !== undefined),
+    ) as Partial<Car>;
+
+    const updatedCar: Car = {
+      ...carDB,
+      ...changes,
+    };
+
+    this.cars = this.cars.map((car) => (car.id === id ? updatedCar : car));
+
+    return updatedCar;
+  }
+
+  delete(id: string) {
+    const carDB = this.findOneById(id);
+
+    const newCars = this.cars.filter((car) => car.id !== carDB.id);
+
+    this.cars = newCars;
+
+    return carDB;
+  }
+}
---
+++ nestjs/o2-car-dealership/src/cars/dto/create-car.dto.ts
@@ -0,0 +1,14 @@
+import { IsString } from 'class-validator';
+
+export class CreateCarDto {
+  @IsString()
+  readonly brand: string;
+
+  @IsString()
+  readonly model: string;
+
+  constructor(brand: string, model: string) {
+    this.brand = brand;
+    this.model = model;
+  }
+}
